193d56152e18cd4de59b1c8f22e084b878bb6abb
Akeel <agilowen@gmail.com>
2011-10-08 17:35:06 -0400

    circuit solver made to read input file of branches

--- Assignment1/circuitSolver.py
+++ Assignment1/circuitSolver.py
@@ -5,79 +5,45 @@ from Choleski import Matrix, Choleski
 def solveCircuit(A,Y,E,J):
 	Choleski(A.multiply(Y).multiply(A.transpose()),A.multiply(J.subtract(Y.multiply(E))))
 
-print "Circuit 1"
-A=Matrix([	[1, -1] ])
-Y=Matrix([	[0.1, 0],
-			[0, 0.1]
-		])		
-E=Matrix([	[0],
-			[10]
-		])
-J=Matrix([	[0],
-			[0]
-		])
-solveCircuit(A,Y,E,J)
+def buildMatrices(filename):
+	global Y, E, J, A
+	
+	#assumes proper formatting (no error detection/correction)
+	f=open(filename)
+	lines=f.readlines()
+	f.close()
 
-		
-print "Circuit 2"
-A=Matrix([	[1, -1] ])
-Y=Matrix([	[0.1, 0],
-			[0, 0.1]
-		])		
-E=Matrix([	[0],
-			[0]
-		])	
-J=Matrix([	[10],
-			[0]
-		])
-solveCircuit(A,Y,E,J)
+	branches=int(lines[0])
+	nodes=int(lines[1])
+
+	Y=Matrix(i=branches,j=branches)
+	E=Matrix(i=branches,j=1)
+	J=Matrix(i=branches,j=1)
+	A=Matrix(i=nodes,j=branches)
+
+	#NB: lines list uses array indexing (starts at 0), but Matrix class uses indexing starting at 1.
+
+	for lineNum in range(2,(branches+1)+1): #skip first two lines already read (adjusted for array indexing, i.e. starts at 0)
+		branch=lineNum-1
+		for i,v in enumerate(lines[lineNum].split(',')):
+			if (i==0):
+				J.set(branch,1,float(v))
+			elif (i==1):
+				Y.set(branch,branch,1/float(v))
+			else:
+				E.set(branch,1,float(v))
+				
+	for lineNum in range((branches+1)+1,(branches+1+nodes)+1):
+		node=lineNum-(branches+1)
+		for j,v in enumerate(lines[lineNum].split(',')):
+			A.set(node,j+1,float(v))
+
+while (True):			
+	filename=raw_input("Enter path to input file(q to quit): ")
+	if (filename=='q'):
+		break
+	buildMatrices(filename)
+	solveCircuit(A,Y,E,J)
 
 
-print "Circuit 4"
-A=Matrix([	[-1, 1, 1, 0],
-			[0, -1, 0, 1]
-		])
-Y=Matrix([	[0.1, 0, 0, 0],
-			[0, 0.2, 0, 0],
-			[0, 0, 0.1, 0],
-			[0, 0, 0, 0.2]
-		])		
-E=Matrix([	[10],
-			[0],
-			[0],
-			[0]
-		])	
-J=Matrix([	[0],
-			[0],
-			[0],
-			[10]
-		])
-solveCircuit(A,Y,E,J)
 
-print "Circuit 5"
-A=Matrix([	[-1, -1, 0, 0, 1, 0],
-			[0, 1, -1, 1, 0, 0],
-			[0, 0, 0, -1, -1, 1]
-		])
-Y=Matrix([	[0.05, 0, 0, 0, 0, 0],
-			[0, 0.1, 0, 0, 0, 0],
-			[0, 0, 0.033333, 0, 0, 0],
-			[0, 0, 0, 0.03333, 0, 0],
-			[0, 0, 0, 0, 0.1, 0],
-			[0, 0, 0, 0, 0, 0.03333]
-		])		
-E=Matrix([	[10],
-			[0],
-			[0],
-			[0],
-			[0],
-			[0]
-		])	
-J=Matrix([	[0],
-			[0],
-			[0],
-			[0],
-			[0],
-			[0]
-		])
-solveCircuit(A,Y,E,J)
